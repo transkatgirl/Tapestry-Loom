cef6cefe3a749d36c2b4edfde6cfb80f5d91579f
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-05-17 17:26:08 -0700

    Started work on WeaveDocument rewrite

--- src/document.ts
+++ src/document.ts
@@ -4,18 +4,98 @@ import { deserialize } from "common";
 import { ulid, ULID } from "ulid";
 import { ModelLabel, UNKNOWN_MODEL_LABEL } from "client";
 
-export interface WeaveDocument {
-	models: Map<ULID, ModelLabel>;
-	nodes: Map<ULID, WeaveDocumentNode>;
+class WeaveDocument {
+	models: Map<ULID, ModelLabel> = new Map();
+	protected nodes: Map<ULID, WeaveDocumentNode> = new Map();
 	currentNode: ULID;
+
+	constructor(content: string) {
+		const identifier = ulid();
+
+		this.nodes.set(identifier, {
+			identifier: identifier,
+			content: content,
+		});
+		this.currentNode = identifier;
+	}
+	getActiveContent(): string {
+		let content = "";
+
+		let node = this.nodes.get(this.currentNode);
+		while (node) {
+			content = node.content + content;
+			if (node.parentNode) {
+				node = this.nodes.get(node.parentNode);
+			} else {
+				node = undefined;
+			}
+		}
+
+		return content;
+	}
+	getActiveNodes(): Array<WeaveDocumentNode> {
+		const nodeList = [];
+
+		let node = this.nodes.get(this.currentNode);
+
+		while (node) {
+			nodeList.push(node);
+			if (node.parentNode) {
+				node = this.nodes.get(node.parentNode);
+			} else {
+				node = undefined;
+			}
+		}
+		nodeList.reverse();
+
+		return nodeList;
+	}
+	getNodeTree() {
+		// TODO
+	}
+	private pruneNodeTree() {
+		// TODO: Prune orphaned nodes; only prune root nodes if they do not have children
+		// TODO: Prune duplicate nodes, combine nodes w/o children
+	}
+	addNode(node: WeaveDocumentNode) {
+		if (node.parentNode) {
+			const parentNode = this.nodes.get(node.parentNode);
+
+			if (parentNode) {
+				if (parentNode.content.length > 0 || !parentNode.parentNode) {
+					this.nodes.set(node.identifier, node);
+				} else {
+					node.parentNode = parentNode.parentNode;
+					this.addNode(node);
+				}
+			} else {
+				node.parentNode = undefined;
+				this.nodes.set(node.identifier, node);
+			}
+		} else {
+			this.nodes.set(node.identifier, node);
+		}
+		if (node.model) {
+			const model = this.models.get(node.model);
+
+			if (!model) {
+				this.models.set(node.model, UNKNOWN_MODEL_LABEL);
+			}
+		}
+	}
+	removeNode(identifier: ULID) {
+		this.nodes.delete(identifier);
+	}
 }
 
 export interface WeaveDocumentNode {
+	identifier: ULID;
 	content: string | Array<[number, string]>;
 	model?: ULID;
 	parentNode?: ULID;
 	metadata?: Map<string, string>;
 }
+
 export const FRONT_MATTER_KEY = "TapestryLoomWeave";
 
 export function loadDocument(editor: Editor) {
@@ -33,7 +113,7 @@ export function loadDocument(editor: Editor) {
 
 		return document;
 	} else {
-		return newDocument(content);
+		return new WeaveDocument(content);
 	}
 }
 
@@ -66,45 +146,14 @@ export function overrideEditorContent(
 	document: WeaveDocument
 ) {}
 
-export function getContent(document: WeaveDocument): string {
-	let content = "";
-
-	let node = document.nodes.get(document.currentNode);
-	while (node) {
-		content = node.content + content;
-		if (node.parentNode) {
-			node = document.nodes.get(node.parentNode);
-		} else {
-			node = undefined;
-		}
-	}
-
-	return content;
-}
-
 function updateDocument(document: WeaveDocument, content: string) {
 	let modified = false;
 
-	const nodeList: Array<WeaveDocumentNode> = [];
-	const identifierList: Array<ULID> = [];
-
-	let node = document.nodes.get(document.currentNode);
-	identifierList.push(document.currentNode);
-	while (node) {
-		nodeList.push(node);
-		if (node.parentNode) {
-			identifierList.push(node.parentNode);
-			node = document.nodes.get(node.parentNode);
-		} else {
-			node = undefined;
-		}
-	}
-	nodeList.reverse();
-	identifierList.reverse();
+	const nodeList = document.getActiveNodes();
 
 	let offset = 0;
 
-	for (const [i, node] of nodeList.entries()) {
+	for (const node of nodeList) {
 		let nodeContent = "";
 
 		if (typeof node.content == "string") {
@@ -126,7 +175,8 @@ function updateDocument(document: WeaveDocument, content: string) {
 			const nodeContent = content.substring(offset);
 
 			if (nodeContent.length > 0 || !node.parentNode) {
-				document.nodes.set(identifier, {
+				document.addNode({
+					identifier: identifier,
 					content: nodeContent,
 					parentNode: node.parentNode,
 				});
@@ -136,9 +186,6 @@ function updateDocument(document: WeaveDocument, content: string) {
 				document.currentNode = node.parentNode;
 			}
 
-			if (identifierList.length == i + 1) {
-				deleteNode(document, identifierList[i]);
-			}
 			offset = offset + nodeContent.length;
 			modified = true;
 			break;
@@ -149,13 +196,15 @@ function updateDocument(document: WeaveDocument, content: string) {
 		const identifier = ulid();
 		const nodeContent = content.substring(offset);
 
-		if (identifierList.length > 0) {
-			appendNode(document, identifier, {
+		if (nodeList.length > 1) {
+			document.addNode({
+				identifier: identifier,
 				content: nodeContent,
-				parentNode: identifierList[identifierList.length - 1],
+				parentNode: nodeList[nodeList.length - 1].identifier,
 			});
 		} else {
-			appendNode(document, identifier, {
+			document.addNode({
+				identifier: identifier,
 				content: nodeContent,
 			});
 		}
@@ -163,67 +212,5 @@ function updateDocument(document: WeaveDocument, content: string) {
 		modified = true;
 	}
 
-	if (modified) {
-		pruneDocument(document);
-	}
-
 	return modified;
-}
-
-function pruneDocument(document: WeaveDocument) {
-	// TODO: Prune orphaned nodes; only prune root nodes if they do not have children
-	// TODO: Prune duplicate nodes, combine nodes w/o children
-}
-
-export function newDocument(content: string): WeaveDocument {
-	const nodes: Map<ULID, WeaveDocumentNode> = new Map();
-
-	const identifier = ulid();
-
-	nodes.set(identifier, {
-		content: content,
-	});
-
-	const document: WeaveDocument = {
-		models: new Map(),
-		nodes: nodes,
-		currentNode: identifier,
-	};
-
-	return document;
-}
-
-export function appendNode(
-	document: WeaveDocument,
-	identifier: ULID,
-	node: WeaveDocumentNode
-) {
-	if (node.parentNode) {
-		const parentNode = document.nodes.get(node.parentNode);
-
-		if (parentNode) {
-			if (parentNode.content.length > 0 || !parentNode.parentNode) {
-				document.nodes.set(identifier, node);
-			} else {
-				node.parentNode = parentNode.parentNode;
-				appendNode(document, identifier, node);
-			}
-		} else {
-			node.parentNode = undefined;
-			document.nodes.set(identifier, node);
-		}
-	} else {
-		document.nodes.set(identifier, node);
-	}
-	if (node.model) {
-		const model = document.models.get(node.model);
-
-		if (!model) {
-			document.models.set(node.model, UNKNOWN_MODEL_LABEL);
-		}
-	}
-}
-
-export function deleteNode(document: WeaveDocument, identifier: ULID) {
-	document.nodes.delete(identifier);
 }
