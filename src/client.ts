9f656e3a0d6b6fee13fd061ca89d0e185ca44a32
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-05-21 07:51:27 -0700

    Started work on inference client

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,7 @@
-import { HexString, requestUrl, RequestUrlParam } from "obsidian";
+import { HexString, requestUrl } from "obsidian";
 import { ULID } from "ulid";
 
 export interface ClientSettings {
-	clientIdentifier?: string;
 	models: Array<ModelConfiguration>;
 }
 
@@ -11,13 +10,12 @@ export enum EndpointType {
 }
 
 export interface ModelConfiguration {
-	baseUrl: string;
-	type: EndpointType;
-	apiToken?: string;
-	modelIdentifier: string;
+	ulid: ULID;
 	label: ModelLabel;
-	customHeaders?: Map<string, string>;
-	customValues?: Map<string, string>;
+	url: string;
+	type: EndpointType;
+	headers?: Record<string, string>;
+	json?: Record<string, string>;
 }
 
 export interface ModelLabel {
@@ -32,21 +30,91 @@ export const UNKNOWN_MODEL_LABEL: ModelLabel = {
 export interface CompletionRequest {
 	prompt: string;
 	count: number;
-	jsonValues?: Map<string, string>;
+	json?: Record<string, string>;
 }
 
 export interface CompletionResponse {
-	top_logprobs: Map<ULID, Array<Array<[number, string]>>>;
-	responses: Map<ULID, Array<string> | Array<Array<[number, string]>>>;
+	model: ModelConfiguration;
+	topProbs?: Array<[number, string]>;
+	completion: string | Array<[number, string]>;
 }
 
 export async function runCompletion(
-	config: ClientSettings,
-	request: Request
-): Promise<Response> {
-	let requests = [];
+	_config: ClientSettings,
+	models: Array<ModelConfiguration>,
+	request: CompletionRequest
+): Promise<Array<CompletionResponse>> {
+	const requests: Array<Promise<Array<CompletionResponse>>> = [];
 
-	for (const model of config.models) {
+	for (const model of models) {
+		const body = {
+			text: request.prompt,
+			...model.json,
+			...request.json,
+		};
+
+		if (model.type == EndpointType.OpenAICompletionv1Compatible) {
+			requests.push(
+				requestUrl({
+					url: model.url,
+					method: "POST",
+					contentType: "application/json",
+					body: JSON.stringify(body),
+					headers: model.headers,
+				}).then((response) => {
+					const responses: Array<CompletionResponse> = [];
+
+					for (const result of response.json["choices"]) {
+						const logprobs = result["logprobs"];
+
+						if (logprobs) {
+							const tokens: Array<[number, string]> = [];
+							for (
+								let i = 0;
+								i < logprobs["tokens"].length;
+								i++
+							) {
+								tokens.push([
+									Math.exp(logprobs["logprob"][i]),
+									logprobs["tokens"][i],
+								]);
+							}
+							const topLogprobs = logprobs["top_logprobs"];
+							if (topLogprobs && topLogprobs.length > 0) {
+								const probs: Array<[number, string]> = [];
+								for (const [token, logprob] of topLogprobs) {
+									probs.push([Math.exp(logprob), token]);
+								}
+								probs.sort((a, b) => {
+									return a[0] - b[0];
+								});
+
+								responses.push({
+									model: model,
+									completion: tokens,
+									topProbs: probs,
+								});
+							} else {
+								responses.push({
+									model: model,
+									completion: tokens,
+								});
+							}
+						} else {
+							responses.push({
+								model: model,
+								completion: result["text"],
+							});
+						}
+					}
+
+					return responses;
+				})
+			);
+		} else {
+			throw new Error("unimplemented!");
+		}
 	}
-	//fetch(endpoint.baseUrl);
+
+	return Promise.all(requests).then((responses) => responses.flat());
 }
