8f616723fda088e47682c9956f94c76bbb99555c
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-06-07 03:35:13 -0700

    Started work on node management

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -20,26 +20,22 @@ pub struct Weave {
 }
 
 impl Weave {
-    pub fn add_node(&mut self, node: Node, parent: Option<Ulid>, model: Option<Model>) -> bool {
+    pub fn add_node(&mut self, node: Node, model: Option<Model>) -> bool {
         if self.nodes.contains_key(&node.id) {
             return false;
         }
-        for identifier in &node.to {
-            if !self.nodes.contains_key(identifier) {
-                return false;
+        if node.from.is_empty() {
+            self.root_nodes.insert(node.id);
+        }
+        for child in node.to {
+            if let Some(entry) = self.nodes.get_mut(&child) {
+                entry.from.insert(node.id);
             }
         }
-        if let Some(parent) = parent {
-            match self.nodes.entry(parent) {
-                Entry::Occupied(mut entry) => {
-                    entry.get_mut().to.insert(node.id);
-                }
-                Entry::Vacant(_entry) => {
-                    return false;
-                }
+        for parent in node.from {
+            if let Some(entry) = self.nodes.get_mut(&parent) {
+                entry.to.insert(node.id);
             }
-        } else {
-            self.root_nodes.insert(node.id);
         }
         if let Some(node_model) = node.content.model() {
             if let Some(model) = model {
@@ -57,16 +53,83 @@ impl Weave {
 
         true
     }
-    pub fn remove_node(&mut self, identifier: &Ulid) -> bool {
-        match self.models.remove(identifier) {
-            Some(node) => {
-                todo!()
+    pub fn update_node_parents(&mut self, identifier: &Ulid, parents: HashSet<Ulid>) {
+        if let Some(old_parents) = self.nodes.get_mut(identifier).map(|node| node.from.clone()) {
+            for parent in &old_parents {
+                if let Some(parent) = self.nodes.get_mut(parent) {
+                    parent.to.remove(identifier);
+                }
             }
-            None => false,
+            for parent in &parents {
+                if let Some(parent) = self.nodes.get_mut(parent) {
+                    parent.to.insert(*identifier);
+                }
+            }
+            match parents.is_empty() {
+                true => self.root_nodes.insert(*identifier),
+                false => self.root_nodes.remove(identifier),
+            };
+            if let Some(node) = self.nodes.get_mut(identifier) {
+                node.from = parents;
+            }
         }
     }
-    pub fn get_node(&self, identifier: &Ulid) -> Option<&Node> {
-        self.nodes.get(identifier)
+    pub fn update_node_children(&mut self, identifier: &Ulid, children: HashSet<Ulid>) {
+        if let Some(old_children) = self.nodes.get_mut(identifier).map(|node| node.to.clone()) {
+            for child in &old_children {
+                if let Some(child) = self.nodes.get_mut(child) {
+                    child.to.remove(identifier);
+                }
+            }
+            for child in &children {
+                if let Some(child) = self.nodes.get_mut(child) {
+                    child.to.insert(*identifier);
+                }
+            }
+            if let Some(node) = self.nodes.get_mut(identifier) {
+                node.to = children;
+            }
+        }
+    }
+    pub fn remove_node(&mut self, identifier: &Ulid, remove_children: bool) {
+        if let Some(node) = self.nodes.remove(identifier) {
+            self.root_nodes.remove(&node.id);
+            for parent in node.from {
+                if let Some(entry) = self.nodes.get_mut(&parent) {
+                    entry.to.remove(&node.id);
+                }
+            }
+            match remove_children {
+                true => {
+                    for child in node.to {
+                        self.remove_node(&child, true);
+                    }
+                }
+                false => {
+                    for child in node.to {
+                        if let Some(entry) = self.nodes.get_mut(&child) {
+                            entry.from.remove(&node.id);
+                        }
+                    }
+                }
+            }
+            if let Some(node_model) = node.content.model() {
+                if let Some(model_nodes) = self.model_nodes.get_mut(&node_model.id) {
+                    model_nodes.remove(&node.id);
+                    if model_nodes.is_empty() {
+                        self.models.remove(&node_model.id);
+                    }
+                }
+            }
+        }
+    }
+    pub fn get_node(&self, identifier: &Ulid) -> (Option<&Node>, Option<&Model>) {
+        let node = self.nodes.get(identifier);
+        let model = node
+            .and_then(|node| node.content.model())
+            .and_then(|node_model| self.models.get(&node_model.id));
+
+        (node, model)
     }
 }
 
@@ -74,6 +137,8 @@ impl Weave {
 pub struct Node {
     pub id: Ulid,
     pub to: HashSet<Ulid>,
+    pub from: HashSet<Ulid>,
+    pub active: bool,
     pub content: NodeContent,
 }
 
