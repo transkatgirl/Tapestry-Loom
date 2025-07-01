cf8a9feca031c555b69b5849f5befc4aade6e838
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-07-01 02:59:41 -0700

    Added additional unit tests

--- weave/src/document/content/tests.rs
+++ weave/src/document/content/tests.rs
@@ -61,3 +61,84 @@ fn apply_modification() {
     .apply(&mut content);
     assert_eq!(content, vec![1, 1, 1, 1, 1, 1, 1, 1]);
 }
+
+#[test]
+fn format_modification_count() {
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 0,
+                insertions: 0,
+                deletions: 0
+            }
+        ),
+        "No Changes"
+    );
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 1,
+                insertions: 1,
+                deletions: 0
+            }
+        ),
+        "1 Insertion"
+    );
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 1,
+                insertions: 0,
+                deletions: 1
+            }
+        ),
+        "1 Deletion"
+    );
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 2,
+                insertions: 1,
+                deletions: 1
+            }
+        ),
+        "1 Insertion, 1 Deletion"
+    );
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 2,
+                insertions: 2,
+                deletions: 0
+            }
+        ),
+        "2 Insertions"
+    );
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 2,
+                insertions: 0,
+                deletions: 2
+            }
+        ),
+        "2 Deletions"
+    );
+    assert_eq!(
+        format!(
+            "{}",
+            ModificationCount {
+                total: 4,
+                insertions: 2,
+                deletions: 2
+            }
+        ),
+        "2 Insertions, 2 Deletions"
+    );
+}
