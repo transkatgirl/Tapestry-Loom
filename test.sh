9e41e606a099f58ae59ff83cc58a9e73d4a2a1b4
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-07-01 12:34:59 -0700

    Minor fixes

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
+rm -r target/coverage/html target/coverage/lcov.info
 grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/lcov.info
 rm cargo-test-*.profraw
 rm loomkit/cargo-test-*.profraw
 rm weave/cargo-test-*.profraw
-#open target/coverage/html/index.html
+open target/coverage/html/index.html
