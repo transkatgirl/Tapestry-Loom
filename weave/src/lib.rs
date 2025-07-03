d6ca5bccc651a59d7b94932472186df64444b4a4
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-07-02 22:07:11 -0700

    Improved tapestry-weave documentation

--- weave/src/lib.rs
+++ weave/src/lib.rs
@@ -5,6 +5,13 @@
 #![warn(missing_docs)]
 
 //! An implementation of Tapestry Loom's "Weave" document format.
+//!
+//! This library implements an interactive in-memory data structure ([`Weave`]) for working with Weave documents, along with a stable binary format for storing Weave documents ([`CompactWeave`]).
 
 pub mod document;
 pub mod format;
+
+#[allow(unused_imports)]
+use document::Weave;
+#[allow(unused_imports)]
+use format::CompactWeave;
