93d1998cfc8c5dbda0654a3f2567a9eeb9652dc9
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-08-04 12:36:46 -0700

    Improved unit tests and roadmap

---
+++ weave/TODO.md
@@ -0,0 +1,11 @@
+# Tapestry Weave Roadmap
+
+## Initial release
+
+See repository README.
+
+## Post-release
+
+- [ ] Optimize Weave::remove_node() to run in linear time in all cases
+- [ ] Optimize WeaveTimeline::annotated_string() to use a rope for annotations instead of a linked list
+- [ ] Add zero-copy Weave format to allow for memory mapping
--- weave/src/document/tests.rs
+++ weave/src/document/tests.rs
@@ -4691,6 +4691,201 @@ fn remove_node() {
         weave.root_nodes,
         HashSet::from([Ulid::from_parts(1, 0), Ulid::from_parts(1, 1)])
     );
+
+    let mut weave = example_weave();
+    assert_eq!(
+        weave.remove_node(&Ulid::from_parts(1, 3)),
+        Some(Node {
+            id: Ulid::from_parts(1, 3),
+            from: HashSet::from([Ulid::from_parts(1, 0), Ulid::from_parts(1, 1)]),
+            to: HashSet::from([Ulid::from_parts(1, 7), Ulid::from_parts(1, 10)]),
+            active: true,
+            bookmarked: false,
+            content: NodeContent::Blank,
+        })
+    );
+    assert_eq!(
+        weave.nodes,
+        HashMap::from([
+            (
+                Ulid::from_parts(0, 0),
+                Node {
+                    id: Ulid::from_parts(0, 0),
+                    from: HashSet::new(),
+                    to: HashSet::from([Ulid::from_parts(0, 1), Ulid::from_parts(0, 2)]),
+                    active: true,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(0, 1),
+                Node {
+                    id: Ulid::from_parts(0, 1),
+                    from: HashSet::from([Ulid::from_parts(0, 0)]),
+                    to: HashSet::new(),
+                    active: true,
+                    bookmarked: true,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(0, 2),
+                Node {
+                    id: Ulid::from_parts(0, 2),
+                    from: HashSet::from([Ulid::from_parts(0, 0)]),
+                    to: HashSet::from([Ulid::from_parts(0, 3)]),
+                    active: true,
+                    bookmarked: true,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(0, 3),
+                Node {
+                    id: Ulid::from_parts(0, 3),
+                    from: HashSet::from([Ulid::from_parts(0, 2)]),
+                    to: HashSet::new(),
+                    active: true,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 0),
+                Node {
+                    id: Ulid::from_parts(1, 0),
+                    from: HashSet::new(),
+                    to: HashSet::from([Ulid::from_parts(1, 2)]),
+                    active: true,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 1),
+                Node {
+                    id: Ulid::from_parts(1, 1),
+                    from: HashSet::new(),
+                    to: HashSet::from([Ulid::from_parts(1, 4)]),
+                    active: true,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 2),
+                Node {
+                    id: Ulid::from_parts(1, 2),
+                    from: HashSet::from([Ulid::from_parts(1, 0)]),
+                    to: HashSet::from([
+                        Ulid::from_parts(1, 5),
+                        Ulid::from_parts(1, 6),
+                        Ulid::from_parts(1, 7)
+                    ]),
+                    active: false,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 4),
+                Node {
+                    id: Ulid::from_parts(1, 4),
+                    from: HashSet::from([Ulid::from_parts(1, 1)]),
+                    to: HashSet::from([Ulid::from_parts(1, 8), Ulid::from_parts(1, 9)]),
+                    active: true,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 5),
+                Node {
+                    id: Ulid::from_parts(1, 5),
+                    from: HashSet::from([Ulid::from_parts(1, 2)]),
+                    to: HashSet::new(),
+                    active: false,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 6),
+                Node {
+                    id: Ulid::from_parts(1, 6),
+                    from: HashSet::from([Ulid::from_parts(1, 2)]),
+                    to: HashSet::from([Ulid::from_parts(1, 10)]),
+                    active: false,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 7),
+                Node {
+                    id: Ulid::from_parts(1, 7),
+                    from: HashSet::from([Ulid::from_parts(1, 2)]),
+                    to: HashSet::from([Ulid::from_parts(1, 10)]),
+                    active: false,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 8),
+                Node {
+                    id: Ulid::from_parts(1, 8),
+                    from: HashSet::from([Ulid::from_parts(1, 4)]),
+                    to: HashSet::new(),
+                    active: false,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 9),
+                Node {
+                    id: Ulid::from_parts(1, 9),
+                    from: HashSet::from([Ulid::from_parts(1, 4)]),
+                    to: HashSet::new(),
+                    active: true,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+            (
+                Ulid::from_parts(1, 10),
+                Node {
+                    id: Ulid::from_parts(1, 10),
+                    from: HashSet::from([Ulid::from_parts(1, 6), Ulid::from_parts(1, 7)]),
+                    to: HashSet::new(),
+                    active: false,
+                    bookmarked: false,
+                    content: NodeContent::Blank,
+                },
+            ),
+        ])
+    );
+    assert!(weave.models.is_empty());
+    assert!(weave.model_nodes.is_empty());
+    assert_eq!(
+        weave.bookmarked_nodes,
+        HashSet::from([Ulid::from_parts(0, 1), Ulid::from_parts(0, 2)])
+    );
+    assert_eq!(
+        weave.multiparent_nodes,
+        HashSet::from([Ulid::from_parts(1, 10)])
+    );
+    assert!(weave.nonconcatable_nodes.is_empty());
+    assert_eq!(
+        weave.root_nodes,
+        HashSet::from([
+            Ulid::from_parts(1, 0),
+            Ulid::from_parts(1, 1),
+            Ulid::from_parts(0, 0)
+        ])
+    );
 }
 
 #[test]
@@ -4715,8 +4910,7 @@ fn remove_inconsistent_node() {
     weave.remove_node(&Ulid::from_parts(0, 0));
 }
 
-#[test]
-fn deduplicate_node() {
+fn example_weave() -> Weave {
     let mut weave = Weave::default();
     weave
         .add_node(
@@ -5133,6 +5327,12 @@ fn deduplicate_node() {
             Ulid::from_parts(0, 0)
         ])
     );
+    weave
+}
+
+#[test]
+fn deduplicate_node() {
+    let mut weave = example_weave();
 
     assert_eq!(weave.deduplicate_node(&Ulid::from_parts(0, 0), false), None);
     assert_eq!(weave.deduplicate_node(&Ulid::from_parts(0, 1), false), None);
