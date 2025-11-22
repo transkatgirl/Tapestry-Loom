d4028612a8e87334d5ed1c7031e4520c9e959340
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-22 06:21:08 -0800

    Started work on API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{relative, FileServer};
+
+#[get("/hello")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from(relative!("static")))
+        .mount("/hello", routes![index])
 }
