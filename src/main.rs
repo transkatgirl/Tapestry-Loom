2d9202121e231e10e10146176934be54875336e8
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-22 07:26:03 -0800

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,16 @@ mod weave;
 
 use rocket::{
     fs::{FileServer, relative},
-    get, routes,
+    routes,
 };
-
-#[get("/hello")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
 
 #[rocket::main]
 #[allow(clippy::result_large_err)]
 async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
+        .manage(weave::WeaveSet::default())
         .mount("/", FileServer::from(relative!("static")))
-        .mount("/v0", routes![index])
+        .mount("/v0", routes![weave::list])
         .mount("/v0", routes![weave::handler])
         .launch()
         .await?;
--- src/weave.rs
+++ src/weave.rs
@@ -1,10 +1,42 @@
-use rocket::{
-    futures::{SinkExt, StreamExt},
-    get,
+use std::{
+    iter,
+    sync::{Arc, Mutex},
 };
 
-#[get("/weave/<id>")]
-pub(super) fn handler(ws: ws::WebSocket, id: u128) -> ws::Channel<'static> {
+use rocket::{
+    State,
+    futures::{SinkExt, StreamExt},
+    get,
+    serde::json::Json,
+};
+use tapestry_weave::v0::TapestryWeave;
+
+#[derive(Default, Debug)]
+pub struct WeaveSet {}
+
+impl WeaveSet {
+    async fn list(&self) -> Vec<rusty_ulid::Ulid> {
+        todo!()
+    }
+    async fn get_or_create_weave(
+        &self,
+        id: rusty_ulid::Ulid,
+    ) -> Result<Arc<Mutex<TapestryWeave>>, String> {
+        todo!()
+    }
+}
+
+#[get("/weaves")]
+pub async fn list(set: &State<WeaveSet>) -> Json<Vec<rusty_ulid::Ulid>> {
+    Json(set.list().await)
+}
+
+#[get("/weaves/<id>")]
+pub fn handler(
+    set: &State<WeaveSet>,
+    ws: ws::WebSocket,
+    id: rusty_ulid::Ulid,
+) -> ws::Channel<'static> {
     ws.channel(move |mut stream| {
         Box::pin(async move {
             while let Some(message) = stream.next().await {
