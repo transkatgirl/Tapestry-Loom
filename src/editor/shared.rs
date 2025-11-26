777f058163828eb5b6d93df5a6183849f8f04a97
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-26 11:57:57 -0800

    Started work on colors

--- src/editor/shared.rs
+++ src/editor/shared.rs
@@ -1,6 +1,6 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
-use eframe::egui::Ui;
+use eframe::egui::{Color32, Rgba, Ui};
 use egui_notify::Toasts;
 use tapestry_weave::{
     ulid::Ulid,
@@ -15,6 +15,7 @@ use crate::settings::Settings;
 pub struct SharedState {
     pub identifier: Ulid,
     pub runtime: Arc<Runtime>,
+    pub cursor_node: Option<Ulid>,
 }
 
 impl SharedState {
@@ -22,10 +23,15 @@ impl SharedState {
         Self {
             identifier,
             runtime,
+            cursor_node: None,
         }
     }
-    pub fn update(&mut self, weave: &mut TapestryWeave, settings: &Settings, toasts: &mut Toasts) {}
-    pub fn reset(&mut self) {}
+    pub fn update(&mut self, weave: &mut TapestryWeave, settings: &Settings, toasts: &mut Toasts) {
+        self.cursor_node = None;
+    }
+    pub fn reset(&mut self) {
+        self.cursor_node = None;
+    }
     pub fn run_inference(
         &mut self,
         weave: &mut TapestryWeave,
@@ -37,10 +43,76 @@ impl SharedState {
 
 pub fn render_node_metadata_tooltip(ui: &mut Ui, node: &DependentNode<NodeContent>) {
     if let Some(model) = &node.contents.model {
-        ui.label(&model.label);
+        if let Some(color) = model
+            .metadata
+            .get("color")
+            .and_then(|h| Color32::from_hex(h).ok())
+        {
+            ui.colored_label(color, &model.label);
+        } else {
+            ui.label(&model.label);
+        }
     }
 
     for (key, value) in &node.contents.metadata {
         ui.label(format!("{key}: {value}"));
+    }
+}
+
+pub fn get_token_color(
+    node_color: Option<Color32>,
+    token_metadata: HashMap<String, String>,
+    settings: &Settings,
+) -> Option<Color32> {
+    if let Some(color) = node_color {
+        if settings.interface.show_token_probabilities
+            && let Some(probability) = token_metadata
+                .get("key")
+                .and_then(|p| p.parse::<f32>().ok())
+        {
+            let probability = probability.clamp(0.0, 1.0);
+            let rgba = Rgba::from(color).to_opaque();
+            let opacity = (1.0 - (f32::log10(1.0 / probability)) / 4.0).min(0.3);
+
+            Some(Color32::from(Rgba::from_rgba_unmultiplied(
+                rgba.r(),
+                rgba.g(),
+                rgba.b(),
+                opacity,
+            )))
+        } else {
+            Some(color)
+        }
+    } else {
+        if settings.interface.show_token_probabilities
+            && let Some(probability) = token_metadata
+                .get("key")
+                .and_then(|p| p.parse::<f32>().ok())
+        {
+            // TODO: Perform color blending in perceptual color space
+            let probability = probability.clamp(0.0, 1.0);
+            let (r, g, b) = if probability > 0.5 {
+                ((1.0 - ((probability - 0.5) * 2.0)) * 255.0, 255.0, 0.0)
+            } else {
+                (255.0, (probability * 2.0) * 255.0, 0.0)
+            };
+
+            Some(Color32::from(Rgba::from_rgb(r, g, b)))
+        } else {
+            None
+        }
+    }
+}
+
+pub fn get_node_color(node: &DependentNode<NodeContent>, settings: &Settings) -> Option<Color32> {
+    if settings.interface.show_model_colors {
+        node.contents.model.as_ref().and_then(|model| {
+            model
+                .metadata
+                .get("color")
+                .and_then(|h| Color32::from_hex(h).ok())
+        })
+    } else {
+        None
     }
 }
