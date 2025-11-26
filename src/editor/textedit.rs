31ea80c9a449ae32835d38d50e696ac6525c9942
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-26 14:32:19 -0800

    Started on scaffolding for text editor

--- src/editor/textedit.rs
+++ src/editor/textedit.rs
@@ -1,4 +1,4 @@
-use eframe::egui::Ui;
+use eframe::egui::{Frame, Id, ScrollArea, TextEdit, TextStyle, Ui};
 use egui_notify::Toasts;
 use tapestry_weave::v0::TapestryWeave;
 
@@ -14,8 +14,21 @@ impl TextEditorView {
         ui: &mut Ui,
         weave: &mut TapestryWeave,
         settings: &Settings,
-        toasts: &mut Toasts,
+        _toasts: &mut Toasts,
         state: &mut SharedState,
     ) {
+        /*ScrollArea::vertical()
+        .auto_shrink(false)
+        .animated(false)
+        .show(ui, |ui| {
+            let textedit = TextEdit::multiline(&mut text)
+                .frame(false)
+                .text_color(ui.visuals().widgets.inactive.text_color())
+                .min_size(ui.available_size())
+                .desired_width(ui.available_size().x)
+                .code_editor();
+
+            if ui.add(textedit).changed() {};
+        });*/
     }
 }
