ce1d21ddf66ccf6df8275559ba8ebe190b0a4ca4
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-28 14:30:06 -0800

    Bugfixes

--- src/editor/textedit.rs
+++ src/editor/textedit.rs
@@ -1,11 +1,12 @@
 use std::{
     cell::RefCell,
+    ops::Range,
     rc::Rc,
     time::{Duration, SystemTime},
 };
 
 use eframe::egui::{
-    Color32, Galley, Pos2, ScrollArea, TextBuffer, TextEdit, TextFormat, TextStyle, Ui,
+    Color32, FontId, Galley, Pos2, ScrollArea, TextBuffer, TextEdit, TextFormat, TextStyle, Ui,
     text::{CCursor, LayoutJob, LayoutSection, TextWrapping},
 };
 use egui_notify::Toasts;
@@ -22,10 +23,11 @@ use crate::{
 
 #[derive(Debug)]
 pub struct TextEditorView {
-    cached_text: String,
-    cached_bytes: Vec<u8>,
-    byte_buffer: Vec<u8>,
-    snippets: Rc<RefCell<Vec<Snippet>>>,
+    text: String,
+    bytes: Vec<u8>,
+    buffer: Vec<u8>,
+    snippets: Vec<Snippet>,
+    highlighting: Rc<RefCell<Vec<LayoutSection>>>,
     last_seen_cursor_node: Option<Ulid>,
     last_seen_hovered_node: Option<Ulid>,
     last_text_edit_cursor: Option<CCursor>,
@@ -33,18 +35,19 @@ pub struct TextEditorView {
     last_text_edit_highlighting_hover: HighlightingHover,
 }
 
-type Snippet = (usize, Ulid, Color32);
+type Snippet = (usize, Ulid, usize);
 
-const SUBSTITUTION_CHAR: char = '␚';
+const SUBSTITUTION_CHAR: char = '␚'; //Must be 1 UTF-8 byte in length
 const SUBSTITUTION_BYTE: u8 = "␚".as_bytes()[0];
 
 impl Default for TextEditorView {
     fn default() -> Self {
         Self {
-            cached_text: String::with_capacity(262144),
-            cached_bytes: Vec::with_capacity(262144),
-            byte_buffer: Vec::with_capacity(262144),
-            snippets: Rc::new(RefCell::new(Vec::with_capacity(65536))),
+            text: String::with_capacity(262144),
+            bytes: Vec::with_capacity(262144),
+            buffer: Vec::with_capacity(262144),
+            snippets: Vec::with_capacity(65536),
+            highlighting: Rc::new(RefCell::new(Vec::with_capacity(65536))),
             last_seen_cursor_node: None,
             last_seen_hovered_node: None,
             last_text_edit_cursor: None,
@@ -56,10 +59,10 @@ impl Default for TextEditorView {
 
 impl TextEditorView {
     pub fn reset(&mut self) {
-        self.cached_text.clear();
-        self.cached_bytes.clear();
-        self.byte_buffer.clear();
-        self.snippets.borrow_mut().clear();
+        self.text.clear();
+        self.bytes.clear();
+        self.buffer.clear();
+        self.snippets.clear();
         self.last_seen_cursor_node = None;
         self.last_seen_hovered_node = None;
         self.last_text_edit_cursor = None;
@@ -74,25 +77,26 @@ impl TextEditorView {
         _toasts: &mut Toasts,
         state: &mut SharedState,
     ) {
+        self.update(
+            weave,
+            settings,
+            ui.visuals().widgets.inactive.text_color(),
+            ui.style().visuals.widgets.hovered.weak_bg_fill,
+            ui.style()
+                .override_font_id
+                .clone()
+                .unwrap_or_else(|| TextStyle::Monospace.resolve(ui.style())),
+        );
+
         if self.last_seen_cursor_node != state.cursor_node {
-            self.update_cache(weave, settings, ui.visuals().widgets.inactive.text_color());
             self.last_seen_cursor_node = state.cursor_node;
         }
 
-        let snippets = self.snippets.clone();
-        let last_hover = self.last_text_edit_highlighting_hover;
+        let sections = self.highlighting.clone();
 
         let mut layouter = |ui: &Ui, buf: &dyn TextBuffer, wrap_width: f32| {
-            let default_color = ui.visuals().widgets.inactive.text_color();
-
             let layout_job = LayoutJob {
-                sections: calculate_highlighting(
-                    ui,
-                    &snippets.borrow(),
-                    buf.as_str().len(),
-                    default_color,
-                    last_hover,
-                ),
+                sections: sections.borrow().clone(),
                 text: buf.as_str().to_string(),
                 wrap: TextWrapping {
                     max_width: wrap_width,
@@ -108,7 +112,7 @@ impl TextEditorView {
             .auto_shrink(false)
             .animated(false)
             .show(ui, |ui| {
-                let textedit = TextEdit::multiline(&mut self.cached_text)
+                let textedit = TextEdit::multiline(&mut self.text)
                     .frame(false)
                     .text_color(ui.visuals().widgets.inactive.text_color())
                     .min_size(ui.available_size())
@@ -119,25 +123,11 @@ impl TextEditorView {
 
                 let top_left = textedit.text_clip_rect.left_top();
 
-                render_boundaries(ui, &self.snippets.borrow(), top_left, &textedit.galley);
+                render_boundaries(ui, &self.snippets, top_left, &textedit.galley);
 
                 if textedit.response.changed() {
                     self.update_weave(weave);
-                    //self.update_cache(weave, settings, ui.visuals().widgets.inactive.text_color());
-                    self.update_snippet_cache(
-                        weave,
-                        settings,
-                        ui.visuals().widgets.inactive.text_color(),
-                    );
-
-                    let position = textedit.cursor_range.map(|c| c.sorted_cursors()[0]);
-                    if let Some((node, _)) = self.calculate_cursor(weave, position.map(|p| p.index))
-                    {
-                        state.cursor_node = Some(node);
-                    } else {
-                        state.cursor_node = None;
-                    }
-                    self.last_text_edit_cursor = position;
+                    self.last_text_edit_cursor = None;
                 } else {
                     let position = textedit.cursor_range.map(|c| c.sorted_cursors()[0]);
                     if position != self.last_text_edit_cursor {
@@ -186,18 +176,23 @@ impl TextEditorView {
                 }
             });
     }
-    fn update_cache(
+    fn update(
         &mut self,
         weave: &mut TapestryWeave,
         settings: &Settings,
         default_color: Color32,
+        hover_bg: Color32,
+        font_id: FontId,
     ) {
-        let mut snippets = self.snippets.borrow_mut();
-        self.cached_text.clear();
-        self.cached_bytes.clear();
-        snippets.clear();
+        let mut highlighting = self.highlighting.borrow_mut();
+        self.text.clear();
+        self.bytes.clear();
+        self.snippets.clear();
+        highlighting.clear();
 
         let active: Vec<u128> = weave.weave.get_active_thread().iter().copied().collect();
+
+        let mut index = 0;
 
         for node in active
             .into_iter()
@@ -208,63 +203,55 @@ impl TextEditorView {
 
             match &node.contents.content {
                 InnerNodeContent::Snippet(snippet) => {
-                    self.cached_bytes.extend_from_slice(snippet);
-                    snippets.push((snippet.len(), Ulid(node.id), color.unwrap_or(default_color)));
+                    let byte_range = index..(index + snippet.len());
+
+                    self.bytes.extend_from_slice(snippet);
+                    self.snippets.push((snippet.len(), Ulid(node.id), index));
+                    highlighting.push(LayoutSection {
+                        leading_space: 0.0,
+                        byte_range: byte_range.clone(),
+                        format: calculate_text_format(
+                            Ulid(node.id),
+                            byte_range,
+                            color.unwrap_or(default_color),
+                            hover_bg,
+                            font_id.clone(),
+                            self.last_text_edit_highlighting_hover,
+                        ),
+                    });
+                    index += snippet.len();
                 }
                 InnerNodeContent::Tokens(tokens) => {
                     for (token, token_metadata) in tokens {
-                        self.cached_bytes.extend_from_slice(token);
-                        snippets.push((
-                            token.len(),
-                            Ulid(node.id),
-                            get_token_color(color, token_metadata, settings)
-                                .unwrap_or(default_color),
-                        ));
+                        let byte_range = index..(index + token.len());
+                        let color = get_token_color(color, token_metadata, settings)
+                            .unwrap_or(default_color);
+
+                        self.bytes.extend_from_slice(token);
+                        self.snippets.push((token.len(), Ulid(node.id), index));
+                        highlighting.push(LayoutSection {
+                            leading_space: 0.0,
+                            byte_range: byte_range.clone(),
+                            format: calculate_text_format(
+                                Ulid(node.id),
+                                byte_range,
+                                color,
+                                hover_bg,
+                                font_id.clone(),
+                                self.last_text_edit_highlighting_hover,
+                            ),
+                        });
+                        index += token.len();
                     }
                 }
             }
         }
 
-        for chunk in self.cached_bytes.utf8_chunks() {
-            self.cached_text.push_str(chunk.valid());
+        for chunk in self.bytes.utf8_chunks() {
+            self.text.push_str(chunk.valid());
 
             for _ in chunk.invalid() {
-                self.cached_text.push(SUBSTITUTION_CHAR);
-            }
-        }
-    }
-    fn update_snippet_cache(
-        &mut self,
-        weave: &mut TapestryWeave,
-        settings: &Settings,
-        default_color: Color32,
-    ) {
-        let mut snippets = self.snippets.borrow_mut();
-        snippets.clear();
-
-        let active: Vec<u128> = weave.weave.get_active_thread().iter().copied().collect();
-
-        for node in active
-            .into_iter()
-            .rev()
-            .filter_map(|id| weave.weave.get_node(&id))
-        {
-            let color = get_node_color(node, settings);
-
-            match &node.contents.content {
-                InnerNodeContent::Snippet(snippet) => {
-                    snippets.push((snippet.len(), Ulid(node.id), color.unwrap_or(default_color)));
-                }
-                InnerNodeContent::Tokens(tokens) => {
-                    for (token, token_metadata) in tokens {
-                        snippets.push((
-                            token.len(),
-                            Ulid(node.id),
-                            get_token_color(color, token_metadata, settings)
-                                .unwrap_or(default_color),
-                        ));
-                    }
-                }
+                self.text.push(SUBSTITUTION_CHAR);
             }
         }
     }
@@ -276,11 +263,11 @@ impl TextEditorView {
         let mut cursor_node = None;
 
         if let Some(char_index) = char_position {
-            let index = self.cached_text.byte_index_from_char_index(char_index);
+            let index = self.text.byte_index_from_char_index(char_index);
 
             let mut offset = 0;
 
-            for (length, node, _) in self.snippets.borrow().iter() {
+            for (length, node, _) in self.snippets.iter() {
                 offset += length;
                 if offset >= index {
                     cursor_node = Some((*node, index));
@@ -290,7 +277,7 @@ impl TextEditorView {
                 }
             }
         } else if let Some(active) = weave.get_active_thread().next().map(|node| Ulid(node.id)) {
-            cursor_node = Some((active, self.cached_text.len()));
+            cursor_node = Some((active, self.text.len()));
         } else {
             cursor_node = None;
         }
@@ -298,22 +285,16 @@ impl TextEditorView {
         cursor_node
     }
     fn update_weave(&mut self, weave: &mut TapestryWeave) {
-        self.byte_buffer.clear();
-        self.byte_buffer
-            .extend_from_slice(self.cached_text.as_bytes());
+        self.buffer.clear();
+        self.buffer.extend_from_slice(self.text.as_bytes());
 
-        for (index, byte) in self
-            .byte_buffer
-            .iter_mut()
-            .take(self.cached_bytes.len())
-            .enumerate()
-        {
+        for (index, byte) in self.buffer.iter_mut().take(self.bytes.len()).enumerate() {
             if *byte == SUBSTITUTION_BYTE {
-                *byte = self.cached_bytes[index];
+                *byte = self.bytes[index];
             }
         }
 
-        weave.set_active_content(&self.byte_buffer, IndexMap::default(), |timestamp| {
+        weave.set_active_content(&self.bytes, IndexMap::default(), |timestamp| {
             if let Some(timestamp) = timestamp {
                 Ulid::from_datetime(SystemTime::UNIX_EPOCH + Duration::from_millis(timestamp))
             } else {
@@ -323,64 +304,31 @@ impl TextEditorView {
     }
 }
 
-fn calculate_highlighting(
-    ui: &Ui,
-    snippets: &[Snippet],
-    length: usize,
-    default_color: Color32,
+fn calculate_text_format(
+    node: Ulid,
+    byte_range: Range<usize>,
+    color: Color32,
+    hover_bg: Color32,
+    font_id: FontId,
     hover: HighlightingHover,
-) -> Vec<LayoutSection> {
-    let font_id = ui
-        .style()
-        .override_font_id
-        .clone()
-        .unwrap_or_else(|| TextStyle::Monospace.resolve(ui.style()));
+) -> TextFormat {
+    let mut format = TextFormat::simple(font_id, color);
 
-    let mut sections = Vec::with_capacity(snippets.len() + 1);
-    let mut index = 0;
-
-    for (snippet_length, node, color) in snippets.iter().copied() {
-        index += snippet_length;
-
-        if index > length {
-            index -= snippet_length;
-            break;
-        }
-
-        let mut format = TextFormat::simple(font_id.clone(), color);
-
-        let byte_range = (index - snippet_length)..index;
-
-        match hover {
-            HighlightingHover::Position((_, hover_position)) => {
-                if byte_range.contains(&hover_position) {
-                    format.background = ui.style().visuals.widgets.hovered.weak_bg_fill;
-                }
+    match hover {
+        HighlightingHover::Position((_, hover_position)) => {
+            if byte_range.contains(&hover_position) {
+                format.background = hover_bg;
             }
-            HighlightingHover::Node(hover_node) => {
-                if hover_node == node {
-                    format.background = ui.style().visuals.widgets.hovered.weak_bg_fill;
-                }
-            }
-            HighlightingHover::None => {}
         }
-
-        sections.push(LayoutSection {
-            leading_space: 0.0,
-            byte_range,
-            format,
-        });
+        HighlightingHover::Node(hover_node) => {
+            if hover_node == node {
+                format.background = hover_bg;
+            }
+        }
+        HighlightingHover::None => {}
     }
 
-    if index < length {
-        sections.push(LayoutSection {
-            leading_space: 0.0,
-            byte_range: index..length,
-            format: TextFormat::simple(font_id, default_color),
-        });
-    }
-
-    sections
+    format
 }
 
 fn render_boundaries(ui: &Ui, snippets: &[Snippet], top_left: Pos2, galley: &Galley) {
