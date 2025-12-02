9e8c271f4b30c7dc0fa146df2eaf27515b75c844
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-12-02 01:10:09 -0800

    UI tweaks

--- src/editor/shared.rs
+++ src/editor/shared.rs
@@ -482,7 +482,7 @@ pub fn get_token_color(
         {
             let probability = probability.clamp(0.0, 1.0);
             let rgba = Rgba::from(color).to_opaque();
-            let opacity = (1.0 - (f32::log10(0.75 / probability)) / 3.0).clamp(0.6, 1.0);
+            let opacity = (1.0 - (f32::log10(0.75 / probability)) / 4.0).clamp(0.66, 1.0);
 
             Some(Color32::from(Rgba::from_rgba_unmultiplied(
                 rgba.r(),
