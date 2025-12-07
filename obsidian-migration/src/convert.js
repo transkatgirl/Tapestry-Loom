262eeb8aec07a4d5a4a6a385939fae76c02e9803
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-12-06 19:30:16 -0800

    Further work on conversion

--- obsidian-migration/src/convert.js
+++ obsidian-migration/src/convert.js
@@ -1,3 +1,18 @@
-let input = eval("(" + input_data + ")");
+const input = eval("(" + input_data + ")");
 
-("");
+const SetCodec = {
+	serialize: (value) => [...value],
+	deserialize: (values) => new Set(values),
+};
+
+const MapCodec = {
+	serialize: (value) => Object.fromEntries(value.entries()),
+	deserialize: (values) => new Map(Object.entries(values)),
+};
+
+JSON.stringify(input, function (key, value) {
+	if (value instanceof Set) return SetCodec.serialize(value);
+	if (value instanceof Map) return MapCodec.serialize(value);
+
+	return value;
+});
