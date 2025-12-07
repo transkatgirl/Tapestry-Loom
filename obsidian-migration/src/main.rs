94e30a84076a488d408139002dfba3384bbb3f51
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-12-06 18:21:22 -0800

    Started work on filesystem handling

--- obsidian-migration/src/main.rs
+++ obsidian-migration/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use clap::Parser;
+use walkdir::WalkDir;
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    /// Folder to scan for weaves created by Tapestry Loom v0
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// Folder to output migrated weaves
+    #[arg(short, long)]
+    output: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    fs::create_dir_all(&args.output)?;
+
+    for entry in WalkDir::new(&args.input) {
+        let entry = entry?;
+        if entry.file_type().is_file()
+            && let Some(extension) = entry.path().extension()
+            && extension.to_ascii_lowercase().to_str() == Some("md")
+        {
+            let mut output = if let Ok(stripped_path) = entry.path().strip_prefix(&args.input) {
+                args.output.clone().join(stripped_path)
+            } else {
+                args.output.clone().join(entry.file_name())
+            };
+            output.set_extension("tapestry");
+
+            if let Some(parent) = output.parent() {
+                fs::create_dir_all(&parent)?;
+            }
+
+            migrate_weave(entry.path(), &output)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn migrate_weave(input: &Path, output: &Path) -> anyhow::Result<()> {
+    let contents = fs::read_to_string(input)?;
+
+    // TODO
+
+    println!("{} -> {}", input.display(), output.display());
+
+    Ok(())
 }
