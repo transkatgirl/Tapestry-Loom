4a841e2e90ede7043716051a713704b24e9f96f2
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-12-14 21:52:08 -0800

    Bugfixes

--- src/editor/textedit.rs
+++ src/editor/textedit.rs
@@ -126,7 +126,7 @@ impl TextEditorView {
     ) {
         if self.text.is_empty() {
             self.update_contents(weave, settings, ui.visuals().widgets.inactive.text_color());
-            ui.ctx().request_repaint();
+            //ui.ctx().request_repaint();
         }
 
         let snippets = self.snippets.clone();
@@ -223,7 +223,7 @@ impl TextEditorView {
                             );
                             self.last_text_edit_rect = textedit.response.rect;
                             self.should_update_rects = false;
-                            ui.ctx().request_repaint();
+                            //ui.ctx().request_repaint();
                         }
 
                         if textedit.response.changed() {
