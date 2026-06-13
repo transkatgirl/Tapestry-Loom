1e822fe362e7fb450b2c3461a619117c2c353f51
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-06-13 14:20:26 -0700

    Updated TODOs

--- tapestry-weave/src/v1/mod.rs
+++ tapestry-weave/src/v1/mod.rs
@@ -4,15 +4,20 @@ pub mod independent;
 pub mod metadata;
 pub mod treeless;
 
-// TODO:
-// - Diff-based tree updates
-// - Prefix-based deduplication?
-// - Support for editor undo/redo
-// - Event-based invalidation support for multi-user weaves
-// - Bookmark labels
-// - Longest common prefix deduplication
-// - Token ID based deduplication
-// - Request parameter based deduplication (especially for single-token nodes)
-// - Add support for temporary nodes which are not actually stored in the IndependentWeave?
+/*
 
-// Useful reference for future formats: https://github.com/transkatgirl/Tapestry-Loom/blob/a232fbbb4119a8a9047ca67a8f1b0cfb772c5bb1/weave/src/document/content/mod.rs
+TODO:
+- Token ID based deduplication
+- Request parameter based deduplication (especially for single-token nodes)
+- Diff-based tree updates
+- Longest common prefix deduplication?
+
+Ideas for future formats:
+- Support for editor undo/redo
+- Event-based invalidation support for multi-user weaves
+- Bookmark labels
+- Add support for temporary nodes which are not actually stored in the IndependentWeave?
+
+See also: https://github.com/transkatgirl/Tapestry-Loom/blob/a232fbbb4119a8a9047ca67a8f1b0cfb772c5bb1/weave/src/document/content/mod.rs
+
+*/
