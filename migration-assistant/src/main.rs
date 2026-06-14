ade6f6ec3d1845a5c2400f36d4160d85a07d7891
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-06-14 08:48:44 -0700

    Bug fixes

--- migration-assistant/src/main.rs
+++ migration-assistant/src/main.rs
@@ -229,6 +229,8 @@ fn migrate_json_weave(
     let created: DateTime<Local> = DateTime::from(fs::metadata(input_path)?.created()?);
 
     if let Ok(weave) = serde_json::from_str::<VersionedInnerWeave>(&input) {
+        println!("{} -> {}", input_path.display(), output_path.display());
+
         return write_weave_to_file(output_path, weave.into_weave(), upgrade, json);
     }
 
