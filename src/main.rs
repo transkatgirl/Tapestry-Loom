70ec32ea8c448da92bb80125474726b39b3ee69e
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-06-22 21:13:06 -0400

    Minor tweaks

--- src/main.rs
+++ src/main.rs
@@ -659,7 +659,13 @@ fn format_large_number_detailed(
     singular_suffix: &str,
     plural_suffix: &str,
 ) -> String {
-    if number >= 100_000_000 {
+    if number >= 100_000_000_000 {
+        format!("{:.0}B {plural_suffix}", number as f32 / 1_000_000_000.0)
+    } else if number >= 10_000_000_000 {
+        format!("{:.1}B {plural_suffix}", number as f32 / 1_000_000_000.0)
+    } else if number >= 1_000_000_000 {
+        format!("{:.2}B {plural_suffix}", number as f32 / 1_000_000_000.0)
+    } else if number >= 100_000_000 {
         format!("{:.0}M {plural_suffix}", number as f32 / 1_000_000.0)
     } else if number >= 10_000_000 {
         format!("{:.1}M {plural_suffix}", number as f32 / 1_000_000.0)
@@ -675,7 +681,11 @@ fn format_large_number_detailed(
 }
 
 fn format_file_size(size: usize) -> String {
-    if size >= 100_000_000_000 {
+    if size >= 100_000_000_000_000 {
+        format!("{:.0} TB", size as f32 / 1_000_000_000_000.0)
+    } else if size >= 1_000_000_000_000 {
+        format!("{:.1} TB", size as f32 / 1_000_000_000_000.0)
+    } else if size >= 100_000_000_000 {
         format!("{:.0} GB", size as f32 / 1_000_000_000.0)
     } else if size >= 1_000_000_000 {
         format!("{:.1} GB", size as f32 / 1_000_000_000.0)
