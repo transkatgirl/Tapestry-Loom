bebf2614573d7fa906fa76de4a0123951dfeaf76
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-06-28 10:16:10 -0700

    Tweaked logging

--- src/common/view.rs
+++ src/common/view.rs
@@ -2,7 +2,7 @@ use eframe::egui::{Context, Ui, WidgetText};
 use egui_tiles::{
     Behavior, Container, SimplificationOptions, Tabs, Tile, TileId, Tiles, Tree, UiResponse,
 };
-use log::warn;
+use log::{debug, warn};
 
 pub struct ViewContainer<T, P>
 where
@@ -254,7 +254,7 @@ where
                     if pane.close(&mut self.behavior.shared) {
                         self.tree.remove_recursively(tile_id);
                     } else {
-                        warn!("View {:?} allowed check_close() but not close()", tile_id);
+                        debug!("View {:?} allowed check_close() but not close()", tile_id);
                         would_close = false;
                     }
                 }
