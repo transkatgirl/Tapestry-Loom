285657027a238b8ebf9c7cb96b75cacd21941ed5
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-09-16 07:52:22 -0700

    Minor fixes & cleanup

--- tapestry-weave/src/migrations/v0.rs
+++ tapestry-weave/src/migrations/v0.rs
@@ -12,7 +12,7 @@ use serde::{Deserialize as SerdeDeserialize, Serialize as SerdeSerialize};
 use ulid::Ulid;
 #[allow(deprecated)]
 use universal_weave::{
-    DiscreteContentResult, DiscreteContents, MetadataWeave, Weave,
+    MetadataWeave, Weave,
     dependent::{DependentWeave, legacy_dependent::DependentWeave as LegacyDependentWeave},
     indexmap::{IndexMap, IndexSet},
     rkyv::{Archive, Deserialize, Serialize},
@@ -39,155 +39,12 @@ pub struct NodeContent {
     pub model: Option<Model>,
 }
 
-impl DiscreteContents for NodeContent {
-    fn len(&self) -> usize {
-        self.content.len()
-    }
-    fn is_empty(&self) -> bool {
-        self.content.is_empty()
-    }
-    fn split(mut self, at: usize) -> DiscreteContentResult<Self> {
-        match self.content.split(at) {
-            DiscreteContentResult::Two(left, right) => {
-                self.content = left;
-
-                let right_content = NodeContent {
-                    content: right,
-                    metadata: self.metadata.clone(),
-                    model: self.model.clone(),
-                };
-
-                DiscreteContentResult::Two(self, right_content)
-            }
-            DiscreteContentResult::One(center) => {
-                self.content = center;
-                DiscreteContentResult::One(self)
-            }
-        }
-    }
-    fn merge(mut self, mut value: Self) -> DiscreteContentResult<Self> {
-        if self.metadata != value.metadata || self.model != value.model {
-            return DiscreteContentResult::Two(self, value);
-        }
-
-        match self.content.merge(value.content) {
-            DiscreteContentResult::Two(left, right) => {
-                self.content = left;
-                value.content = right;
-
-                DiscreteContentResult::Two(self, value)
-            }
-            DiscreteContentResult::One(center) => {
-                self.content = center;
-                DiscreteContentResult::One(self)
-            }
-        }
-    }
-}
-
 #[derive(
     SerdeSerialize, SerdeDeserialize, Archive, Deserialize, Serialize, Debug, Clone, PartialEq, Eq,
 )]
 pub enum InnerNodeContent {
     Snippet(Vec<u8>),
     Tokens(Vec<(Vec<u8>, MetadataMap)>),
-}
-
-impl InnerNodeContent {
-    fn len(&self) -> usize {
-        match self {
-            Self::Snippet(snippet) => snippet.len(),
-            Self::Tokens(tokens) => tokens.iter().map(|token| token.0.len()).sum(),
-        }
-    }
-    fn is_empty(&self) -> bool {
-        match self {
-            Self::Snippet(snippet) => snippet.is_empty(),
-            Self::Tokens(tokens) => tokens.iter().all(|token| token.0.is_empty()),
-        }
-    }
-    fn split(self, at: usize) -> DiscreteContentResult<Self> {
-        if at == 0 {
-            return DiscreteContentResult::One(self);
-        }
-
-        match self {
-            Self::Snippet(mut snippet) => {
-                if snippet.len() <= at {
-                    return DiscreteContentResult::One(Self::Snippet(snippet));
-                }
-
-                let right = snippet.split_off(at);
-                snippet.shrink_to_fit();
-
-                DiscreteContentResult::Two(Self::Snippet(snippet), Self::Snippet(right))
-            }
-            Self::Tokens(tokens) => {
-                if tokens.iter().map(|token| token.0.len()).sum::<usize>() <= at {
-                    return DiscreteContentResult::One(Self::Tokens(tokens));
-                }
-
-                let mut content_index = 0;
-
-                let location = tokens.iter().enumerate().find_map(|(location, token)| {
-                    if content_index + token.0.len() > at {
-                        return Some(location);
-                    }
-                    content_index += token.0.len();
-
-                    None
-                });
-
-                if let Some(location) = location {
-                    let mut left = tokens;
-                    let mut right = left.split_off(location);
-                    left.shrink_to_fit();
-
-                    let mut left_token = right[0].0.clone();
-                    let right_token = left_token.split_off(at - content_index);
-
-                    debug_assert!(!right_token.is_empty() || left_token.is_empty());
-
-                    if !left_token.is_empty() {
-                        left_token.shrink_to_fit();
-                        let mut left_metadata = right[0].1.clone();
-                        left_metadata.shift_remove("token_id");
-                        left.push((left_token, left_metadata));
-                        right[0].1.shift_remove("token_id");
-                    }
-                    right[0].0 = right_token;
-
-                    DiscreteContentResult::Two(Self::Tokens(left), Self::Tokens(right))
-                } else {
-                    DiscreteContentResult::One(Self::Tokens(tokens))
-                }
-            }
-        }
-    }
-    fn merge(self, value: Self) -> DiscreteContentResult<Self> {
-        match self {
-            Self::Snippet(mut left_snippet) => match value {
-                Self::Snippet(mut right_snippet) => {
-                    left_snippet.append(&mut right_snippet);
-                    DiscreteContentResult::One(Self::Snippet(left_snippet))
-                }
-                Self::Tokens(right_tokens) => DiscreteContentResult::Two(
-                    Self::Snippet(left_snippet),
-                    Self::Tokens(right_tokens),
-                ),
-            },
-            Self::Tokens(mut left_tokens) => match value {
-                Self::Snippet(right_snippet) => DiscreteContentResult::Two(
-                    Self::Tokens(left_tokens),
-                    Self::Snippet(right_snippet),
-                ),
-                Self::Tokens(mut right_tokens) => {
-                    left_tokens.append(&mut right_tokens);
-                    DiscreteContentResult::One(Self::Tokens(left_tokens))
-                }
-            },
-        }
-    }
 }
 
 #[derive(
@@ -203,6 +60,8 @@ pub type TapestryWeave =
     LegacyDependentWeave<u128, NodeContent, MetadataMap, BuildHasherDefault<RandomIdHasher>>;
 //pub type TapestryNode = DependentNode<u128, NodeContent, BuildHasherDefault<RandomIdHasher>>;
 pub type MetadataMap = IndexMap<String, String, RandomState>;
+
+const MIN_PROBABILITY: f32 = 0.00005;
 
 pub fn deserialize_counterfactual_logprobs(logprobs: &str) -> Option<Vec<(Vec<u8>, MetadataMap)>> {
     serde_json::from_str::<Vec<(String, MetadataMap)>>(logprobs)
@@ -242,10 +101,12 @@ impl From<InnerNodeContent> for NewInnerNodeContent {
 
                         InnerNodeToken {
                             bytes: token,
-                            logprob: metadata
-                                .shift_remove("probability")
-                                .and_then(|value| value.parse::<f32>().ok())
-                                .map(|prob| prob.ln()),
+                            logprob: metadata.shift_remove("probability").and_then(|value| {
+                                value
+                                    .parse::<f32>()
+                                    .ok()
+                                    .map(|p| p.max(MIN_PROBABILITY).ln())
+                            }),
                             id: if !modified {
                                 metadata
                                     .shift_remove("token_id")
@@ -265,8 +126,11 @@ impl From<InnerNodeContent> for NewInnerNodeContent {
                                                     bytes: token,
                                                     logprob: metadata
                                                         .shift_remove("probability")
-                                                        .and_then(|value| value.parse::<f32>().ok())
-                                                        .map(|prob| prob.ln()),
+                                                        .and_then(|value| {
+                                                            value.parse::<f32>().ok().map(|p| {
+                                                                p.max(MIN_PROBABILITY).ln()
+                                                            })
+                                                        }),
                                                     id: metadata.shift_remove("token_id").and_then(
                                                         |value| value.parse::<u64>().ok(),
                                                     ),
